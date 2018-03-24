b7e14cbf09d0b251ee53f2e3661da2d39a28c369
Gurpreet Singh <g.singh6343@gmail.com>
2018-03-25 00:29:52 +0530

    Cleaned;

--- code/config.py
+++ code/config.py
@@ -1,9 +1,9 @@
 n_clusters = 10
 
 input_dim = 784
-latent_dim = 2
+latent_dim = 10
 
-n_epochs = 100
+n_epochs = 3000
 batch_size = 100
 
 regularizer = 1
@@ -11,6 +11,7 @@ regularizer = 1
 encoder_hidden_size = [500, 500, 2000]
 decoder_hidden_size = [2000, 500, 500]
 
-global_step = 0
-adam_nn_learning_rate = 0.002
-adam_nn_epsilon = 1e-04
+adam_decay_steps = 10
+adam_decay_rate = 0.9
+adam_learning_rate = 0.002
+adam_epsilon = 1e-04
