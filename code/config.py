72874c49ea667e2dae31a8f3a2e1b32f6c9916ae
Gurpreet Singh <g.singh6343@gmail.com>
2018-03-24 23:55:24 +0530

    Made everything trainable (probably working); Cleaned

--- code/config.py
+++ code/config.py
@@ -3,8 +3,10 @@ n_clusters = 10
 input_dim = 784
 latent_dim = 2
 
-n_epochs = 50
+n_epochs = 100
 batch_size = 100
+
+regularizer = 1
 
 encoder_hidden_size = [500, 500, 2000]
 decoder_hidden_size = [2000, 500, 500]
